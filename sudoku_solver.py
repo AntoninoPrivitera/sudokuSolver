bc6be3a249f65dd3c4ff6248392b54019a25702f
MrJakBlade30 <antoprivit96@gmail.com>
2021-12-21 23:09:36 +0100

    added terminal

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,15 +1,31 @@
 from sudoku_lib import ImportSudokuData, PrintSudokuInfo, SudokuSolveZ3, SudokuSolveBacktracking;
 
+while True:
+    print("COMMANDS:")
+    print("1 - solve a sudoku from a file")
+    print("2 - generate a sudoku")
+    print("3 - exit")
+    value = input("Please enter a number to run the command: ")
+    match value:
+        case "1":
+            print("You selected 1 to solve a sudoku from a file")
+            filename = input("Please add the file name which contains your sudoku: ")
+            gridSize, grid = ImportSudokuData("sudoku_data.txt")
 
-#create terminal
-value = input("Please enter a string: ")
-print(f'You entered {value}')
+            print("your file contains the following sudoku to solve:")
+            PrintSudokuInfo(gridSize, grid)
 
-gridSize, grid = ImportSudokuData("sudoku_data.txt")
-
-print("your file contains the following sudoku to solve:")
-PrintSudokuInfo(gridSize, grid)
-
-SudokuSolveZ3(gridSize, grid)
-
-SudokuSolveBacktracking(gridSize, grid)
+            solver = input("Which solver do you want to use? (1 - z3, 2 - backtracking): ")
+            if solver == "1":
+                SudokuSolveZ3(gridSize, grid)
+            elif solver == "2":
+                SudokuSolveBacktracking(gridSize, grid)
+            else:
+                print("Error in the input to select the solver")
+        case "2":
+            print("You selected 2 to generate a sudoku")
+        case "3":
+            print("You selected 3 to exit")
+            break
+        case _:
+            print("Error in the input to select the command")
