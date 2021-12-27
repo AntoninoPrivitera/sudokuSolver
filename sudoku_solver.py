e1b28271c278a7647913b1ac6575628fcdb6906a
Concetto Antonino Privitera <antoprivit96@gmail.com>
2021-12-27 17:25:27 +0100

    moved from match to if for compatibility with python lower than 3.9

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -6,51 +6,50 @@ while True:
     print("2 - generate a sudoku")
     print("3 - exit")
     value = input("Please enter a number to run the command: ")
-    match value: #required python 3.10 or newer
-        case "1":
-            print("You selected 1 to solve a sudoku from a file")
-            filename = input("Please add the file name which contains your sudoku (empty if you want to use sudoku_data.txt as default): ")
+    if value == "1":
+        print("You selected 1 to solve a sudoku from a file")
+        filename = input("Please add the file name which contains your sudoku (empty if you want to use sudoku_data.txt as default): ")
+        
+        try:
+            gridSize, grid = ImportSudokuData("sudoku_data.txt" if filename == "" else filename)
+
+            print("your file contains the following sudoku to solve:")
+            PrintSudokuInfo(gridSize, grid)
+
+            solver = input("Which solver do you want to use? (1 - z3, 2 - backtracking): ")
+            if solver == "1":
+                SudokuSolveZ3(gridSize, grid)
+            elif solver == "2":
+                SudokuSolveBacktracking(gridSize, grid)
+            else:
+                print("Error in the input to select the solver")
+        except Exception as e:
+            print(e)
+    elif value == "2":
+        print("You selected 2 to generate a sudoku")
+        print("Difficulties:")
+        print("1 - EASY")
+        print("2 - MEDIUM")
+        print("3 - HARD")
+        input_difficulty = input("Please select your difficulty: ")
+        try:
+            difficulty = int(input_difficulty)
+            if difficulty < 1 or difficulty > 3:
+                print("Error in the input to select the difficulty")
+            else:
+                print("Grid size available:")
+                print("4 - 4x4")
+                print("9 - 9x9")
+                print("16 - 16x16")
+                print("25 - 25x25")
+                input_gridSize = input("Choose the grid size:")
+                GenerateSudoku(int(input_gridSize), Difficulty(difficulty))
+        except Exception as e:
+            print(e)
+
             
-            try:
-                gridSize, grid = ImportSudokuData("sudoku_data.txt" if filename == "" else filename)
-
-                print("your file contains the following sudoku to solve:")
-                PrintSudokuInfo(gridSize, grid)
-
-                solver = input("Which solver do you want to use? (1 - z3, 2 - backtracking): ")
-                if solver == "1":
-                    SudokuSolveZ3(gridSize, grid)
-                elif solver == "2":
-                    SudokuSolveBacktracking(gridSize, grid)
-                else:
-                    print("Error in the input to select the solver")
-            except Exception as e:
-                print(e)
-        case "2":
-            print("You selected 2 to generate a sudoku")
-            print("Difficulties:")
-            print("1 - EASY")
-            print("2 - MEDIUM")
-            print("3 - HARD")
-            input_difficulty = input("Please select your difficulty: ")
-            try:
-                difficulty = int(input_difficulty)
-                if difficulty < 1 or difficulty > 3:
-                    print("Error in the input to select the difficulty")
-                else:
-                    print("Grid size available:")
-                    print("4 - 4x4")
-                    print("9 - 9x9")
-                    print("16 - 16x16")
-                    print("25 - 25x25")
-                    input_gridSize = input("Choose the grid size:")
-                    GenerateSudoku(int(input_gridSize), Difficulty(difficulty))
-            except Exception as e:
-                print(e)
-
-            
-        case "3":
-            print("You selected 3 to exit")
-            break
-        case _:
-            print("Error in the input to select the command")
+    elif "3":
+        print("You selected 3 to exit")
+        break
+    else:
+        print("Error in the input to select the command")
